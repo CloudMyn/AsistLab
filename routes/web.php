c3c64529ee09d12904c48001d13a47c64b0405b9
cloudmyn <cloudmyn46@gmail.com>
2024-12-05 19:09:20 +0800

    update web

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    if (auth()->guest()) {
-        return redirect()->intended('/praktikan/login');
+    $domains = [
+        'asisten' => env('ASISTEN_DOMAIN', null),
+        'admin' => env('ADMIN_DOMAIN', null),
+        'kepala_lab' => env('KEPALA_LAB_DOMAIN', null),
+        'praktikan' => env('PRAKTIKAN_DOMAIN', null),
+        'developer' => env('DEVELOPER_DOMAIN', null),
+    ];
+
+    if (array_key_exists(request()->getHost(), $domains)) {
+        if (auth()->guest()) {
+            return redirect()->intended("/{$domains[request()->getHost()]}/login");
+        }
     }
 
     $user   =   get_auth_user();
