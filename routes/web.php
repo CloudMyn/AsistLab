0a8934c89bb6b2cbb6c82790cdfdebcb93b36b03
cloudmyn <cloudmyn46@gmail.com>
2024-12-05 18:15:49 +0800

    update main page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    if (auth()->guest()) {
+        return redirect()->intended('/praktikan/login');
+    }
+
+    $user   =   get_auth_user();
+
+    return match($user->roles()->first()->name) {
+        'developer' => redirect()->route('filament.developer.pages.dashboard'),
+        'admin' => redirect()->route('filament.admin.pages.dashboard'),
+        'praktikan' => redirect()->route('filament.praktikan.pages.dashboard'),
+        'asisten' => redirect()->route('filament.asisten.pages.dashboard'),
+        'kepala_lab' => redirect()->route('filament.kepala_lab.pages.dashboard'),
+        default => redirect()->route('filament.praktikan.pages.dashboard'),
 });
