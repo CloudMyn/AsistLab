fd038de8c4cbd4146fc45500f1d2dc517612becc
cloudmyn <cloudmyn46@gmail.com>
2024-12-05 19:22:54 +0800

    fix update

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,16 @@ Route::get('/', function () {
     $domains = [
         'asisten' => env('ASISTEN_DOMAIN', null),
         'admin' => env('ADMIN_DOMAIN', null),
-        'kepala_lab' => env('KEPALA_LAB_DOMAIN', null),
+        'kepala-lab' => env('KEPALA_LAB_DOMAIN', null),
         'praktikan' => env('PRAKTIKAN_DOMAIN', null),
         'developer' => env('DEVELOPER_DOMAIN', null),
     ];
 
-    dd($domains, request()->getHost(), array_key_exists(request()->getHost(), $domains));
+    $host = request()->getHost();
+    $domainKey = array_search($host, $domains);
 
-    if (array_key_exists(request()->getHost(), $domains)) {
-        return redirect()->intended("/{$domains[request()->getHost()]}/login");
+    if ($domainKey !== false) {
+        return redirect()->intended("/{$domains[$domainKey]}/{$domainKey}/login");
     } else {
         return redirect()->route('filament.praktikan.auth.login');
     }
