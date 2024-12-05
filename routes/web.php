61addcf100a6360241f957842bbeaf34d57d1c13
cloudmyn <cloudmyn46@gmail.com>
2024-12-05 18:16:41 +0800

    fix error

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,12 @@ Route::get('/', function () {
 
     $user   =   get_auth_user();
 
-    return match($user->roles()->first()->name) {
+    return match ($user->roles()->first()->name) {
         'developer' => redirect()->route('filament.developer.pages.dashboard'),
         'admin' => redirect()->route('filament.admin.pages.dashboard'),
         'praktikan' => redirect()->route('filament.praktikan.pages.dashboard'),
         'asisten' => redirect()->route('filament.asisten.pages.dashboard'),
         'kepala_lab' => redirect()->route('filament.kepala_lab.pages.dashboard'),
-        default => redirect()->route('filament.praktikan.pages.dashboard'),
+        default => redirect()->route('filament.admin.pages.dashboard'),
+    };
 });
